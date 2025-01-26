4f01124d90bae2f43345d36b8cb70a6bea74e1f4
YashRaj1506 <yashraj504300@gmail.com>
2025-01-26 12:48:25 +0530

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -12,7 +12,9 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message,err := greetings.Hello("Yash")
+	names := []string{"Yash","Samantha","Rachel"}
+
+	message,err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
