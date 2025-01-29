1c054fb70029ea1800df7df4af5ad525529eb22a
YashRaj1506 <yashraj504300@gmail.com>
2025-01-29 13:09:14 +0530

    learned stucts with inherti and embed features

--- components/component.go
+++ components/component.go
@@ -9,7 +9,53 @@ type car struct {
 	Model string
 	Height int
 	Width int
+	Frontwheel Wheel //Nester struct
+	BackWheel Wheel
 }
+
+type Wheel struct {
+	Radius int
+	Material string
+}
+
+// Anonymous Structs
+// myCar := car{}
+// myCar.Frontwheel.Radius = 5
+
+// myCar := struct {
+// 	Make string
+//	Model string
+//	} {
+//		Make : "tesla",
+//		Model : "model 3"
+//	}
+
+//inheriting fields from one stryuct to another
+
+type car struct {
+	make string
+	model string
+}
+
+type truck struct {
+	// "car" is embedded, so tej defination of a
+	// "truck" now also additionally contains all
+	// of the fields of the car struct
+    car
+	bedsize int
+}
+
+// now instead of truck.car.model we just go for truck.model
+
+lanesTruck = truck {
+	bedsize: 10,
+	car: car{
+		make : "toyota",
+		model: "camry",
+	},
+}
+
+
 
 
 func top(x int ) int {
