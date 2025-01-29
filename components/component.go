1a3f4837607504d5183fe73dcb8cd7ec657a45cf
YashRaj1506 <yashraj504300@gmail.com>
2025-01-29 12:55:14 +0530

    struct basic

--- components/component.go
+++ components/component.go
@@ -4,23 +4,41 @@ import (
 	"fmt"
 )
 
+type car struct {
+	Make string
+	Model string
+	Height int
+	Width int
+}
+
+
+func top(x int ) int {
+	return x + 1
+}
+
 func main() {
-	var sms int
-	var consti float64
-	var per bool
-	var username string
+	// var sms int
+	// var consti float64
+	// var per bool
+	// var username string
 
-	fmt.Printf(
-		"%v %f %v %q\n",
-		sms,
-		consti,
-		per,
-		username,
-	)
+	// fmt.Printf(
+	// 	"%v %f %v %q\n",
+	// 	sms,
+	// 	consti,
+	// 	per,
+	// 	username,
+	// )
 
-	if length := getLength(email); length < 1 {
-		fmt.Println("Hye")
-	}
+	// if length := getLength(email); length < 1 {
+	// 	fmt.Println("Hye")
+	// }
+
+	a := top(5)
+	fmt.Println(a)
 
 	// var empty string or empty:= ""
+
 }
+
+
