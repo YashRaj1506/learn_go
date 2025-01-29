3529876e7bf5e5bce0f031d778b144f7feb77162
YashRaj1506 <yashraj504300@gmail.com>
2025-01-29 12:33:12 +0530

    functions done

---
+++ components/component.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var sms int
+	var consti float64
+	var per bool
+	var username string
+
+	fmt.Printf(
+		"%v %f %v %q\n",
+		sms,
+		consti,
+		per,
+		username,
+	)
+
+	if length := getLength(email); length < 1 {
+		fmt.Println("Hye")
+	}
+
+	// var empty string or empty:= ""
+}
---
+++ components/go.mod
@@ -0,0 +1,3 @@
+module learn_go/components
+
+go 1.23.4
